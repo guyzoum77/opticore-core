56b2ee285353f63ca943127482a79e08e4801774
guyzoum77 <guyzoum7700@mail.com>
2024-06-22 12:14:23 +0000

    using colors-ansi into logger.utils

--- src/core/utils/logs/logger.utils.ts
+++ src/core/utils/logs/logger.utils.ts
@@ -1,7 +1,7 @@
 import { injectable } from "inversify";
 import winston, { format } from "winston";
 import { LogLevelEnum } from "../../../domain/enums/logLevel.enum";
-import {DailyRotateFile, fs } from "../../..";
+import {colors, DailyRotateFile, fs} from "../../..";
 
 
 export type LogMessage = string;
@@ -21,22 +21,22 @@ export default class LoggerUtils {
         this._createLogDir();
     }
 
-    public logInfo(msg: LogMessage, context?: LogContext): void {
-        this._logInfo(msg, LogLevelEnum.INFO, context);
+    public logInfo(msg: LogMessage, context?: LogContext) {
+        this._log(msg, LogLevelEnum.INFO, context);
     }
-    public logWarn(msg: LogMessage, context?: LogContext): void {
-        this._logWarn(msg, LogLevelEnum.WARN, context);
+    public logWarn(msg: LogMessage, context?: LogContext) {
+        this._log(msg, LogLevelEnum.WARN, context);
     }
-    public logError(msg: LogMessage, context?: LogContext): void {
-        this._logError(msg, LogLevelEnum.ERROR, context);
+    public logError(msg: LogMessage, context?: LogContext) {
+        this._log(msg, LogLevelEnum.ERROR, context);
     }
-    public logDebug(msg: LogMessage, context?: LogContext): void {
+    public logDebug(msg: LogMessage, context?: LogContext) {
         if (process.env.NODE_ENV !== "production") {
-            this._logDebug(msg, LogLevelEnum.DEBUG, context); // Don"t log debug in production
+            this._log(msg, LogLevelEnum.DEBUG, context); // Don"t log debug in production
         }
     }
 
-    public createLogInFile(msg: LogMessage, context?: LogContext): winston.Logger {
+    public createLogInFile(msg: LogMessage, context?: LogContext) {
         return winston.createLogger({
             transports: LoggerUtils._getTransports("development", "dev"),
         });
@@ -53,26 +53,30 @@ export default class LoggerUtils {
         }
     }
 
-    private _logInfo(msg: LogMessage, level: LogLevelEnum, context?: LogContext): void {
-        this._logger.info(level, msg, {context});
-    }
-    private _logWarn(msg: LogMessage, level: LogLevelEnum, context?: LogContext): void {
-        this._logger.alert(level, msg, {context});
-    }
-    private _logError(msg: LogMessage, level: LogLevelEnum, context?: LogContext): void {
-        this._logger.error(level, msg, {context});
-    }
-    private _logDebug(msg: LogMessage, level: LogLevelEnum, context?: LogContext): void {
-        this._logger.debug(level, msg, {context});
+    private _log(msg: LogMessage, level: LogLevelEnum, context?: LogContext) {
+        switch (LogLevelEnum) {  // @ts-ignore
+            case LogLevelEnum.DEBUG:
+                this._logger.debug(level, msg, {context});
+                break; // @ts-ignore
+            case LogLevelEnum.WARN:
+                this._logger.warn(level, msg, {context});
+                break; // @ts-ignore
+            case LogLevelEnum.ERROR:
+                this._logger.error(level, msg, {context});
+                break; // @ts-ignore
+            case LogLevelEnum.INFO:
+                this._logger.info(level, msg, {context});
+                break;
+        }
     }
 
-    private _initializeWinston(): winston.Logger {
+    private _initializeWinston() {
         return winston.createLogger({
             transports: LoggerUtils._getTransports("production", "prod"),
         });
     }
 
-    private static _getTransports(env: string, logFileName: string): any[] {
+    private static _getTransports(env: string, logFileName: string) {
         const transports: any[] = [
             new winston.transports.Console({
                 format: this._getFormatForConsole(),
@@ -86,20 +90,17 @@ export default class LoggerUtils {
         return transports;
     }
 
-    private static _getFormatForConsole(): winston.Logform.Format {
+    private static _getFormatForConsole() {
         return format.combine(
-            format.errors({stack: true}),
-            format.splat(),
             format.timestamp(),
             format.printf((info: any) => {
-                return `${new Date(info.timestamp)} | [${info.level}] | [CONTEXT] ${info.message} | ${new Date(info.timestamp).toLocaleTimeString()}`
+                return `${colors.blue(`${new Date(info.timestamp)}`)} [${info.level}] ${info.message} ${new Date(info.timestamp).toLocaleTimeString()}`
             }),
-            format.colorize({ all: true , level: true, message: true })
-            //colors: { info: 'green', warn: 'cyan', error: 'red' }
+            format.colorize({all: true})
         );
     }
 
-    private static _getFileTransport(logFileName: string): DailyRotateFile {
+    private static _getFileTransport(logFileName: string) {
         return new DailyRotateFile({
             filename: `${logFileName}-%DATE%.log`,
             dirname: this.logDirectory,
