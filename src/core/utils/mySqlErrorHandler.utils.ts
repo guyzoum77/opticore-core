eb3dc38cced5d10a8c32546e5455046f25479ae0
guyzoum77 <guyzoum7700@mail.com>
2024-07-01 18:14:12 +0000

    fixing all errors into mySqlErrorHandler utils file

--- src/core/utils/mySqlErrorHandler.utils.ts
+++ src/core/utils/mySqlErrorHandler.utils.ts
@@ -1,46 +1,40 @@
 import {colors, Exception, ExceptionHandlerError, HttpStatusCodesConstant, LoggerComponent, mySQL} from "../../index";
+import chalk from "chalk";
 
 export function mySqlErrorHandlerUtils(err: mySQL.MysqlError, dbHost?: string | null,
                                        database?: string | null, user?: string | null,
                                        password?: string | null): void {
     const dateTime: string = `${(new Date().getMonth())}-${(new Date().getDate())}-${(new Date().getFullYear())} ${(new Date().getHours())}:${(new Date().getMinutes())}:${(new Date().getSeconds())}`;
-
     switch (err.code) {
         case "EAI_AGAIN":
             LoggerComponent.logErrorMessage(Exception.errorDBHost(dbHost!), Exception.mysqlErrorCon);
-            throw new ExceptionHandlerError(
-                `${colors.bgRed(`${colors.white(err.message +`\n${Exception.erNotSupportedAuthMode}`)}`)}`,
-                `${colors.red(Exception.mysqlErrorCon)}`,
-                HttpStatusCodesConstant.UNAUTHORIZED,
-                true
-            );
+            logErrorName();
+            console.log(`[ ${colors.red(`${colors.bold('DataBase connection')}`)} ] | ${dateTime} | [ ${colors.red(`${colors.bold(`error`)}`)} ] | [ ${colors.bold(`Type error`)} ] ${colors.red(`${Exception.mysqlErrorCon} : ${colors.bold(`EAI_AGAIN`)}`)} - [ ${colors.bold(`Error message`)} ] ${colors.red(`${Exception.errorDBHost(dbHost!)}`)} - [ ${colors.red(`${colors.bold(`HttpCode`)}`)} ] ${HttpStatusCodesConstant.UNAUTHORIZED}`);
+            break;
         case "ER_NOT_SUPPORTED_AUTH_MODE":
             LoggerComponent.logErrorMessage(Exception.erNotSupportedAuthMode, Exception.mysqlErrorCon);
-            throw new ExceptionHandlerError(
-                `${colors.bgRed(`${colors.white(err.message +`\n${Exception.erNotSupportedAuthMode}`)}`)}`,
-                `${colors.red(Exception.mysqlErrorCon)}`,
-                HttpStatusCodesConstant.UNAUTHORIZED,
-                true
-            );
+            logErrorName();
+            console.log(`[ ${colors.red(`${colors.bold('DataBase connection')}`)} ] | ${dateTime} | [ ${colors.red(`${colors.bold(`error`)}`)} ] | [ ${colors.bold(`Type error`)} ] ${colors.red(`${Exception.mysqlErrorCon} : ${colors.bold(`ER_NOT_SUPPORTED_AUTH_MODE`)}`)} - [ ${colors.bold(`Error message`)} ] ${colors.red(`${Exception.erNotSupportedAuthMode}`)} - [ ${colors.red(`${colors.bold(`HttpCode`)}`)} ] ${HttpStatusCodesConstant.UNAUTHORIZED} `)
+            break;
         case "ER_ACCESS_DENIED_ERROR":
             LoggerComponent.logErrorMessage(Exception.accessDeniedToDBCon(user!, password!), Exception.mysqlErrorCon);
+            logErrorName();
             console.log(`[ ${colors.red(`${colors.bold('DataBase connection')}`)} ] | ${dateTime} | [ ${colors.red(`${colors.bold(`error`)}`)} ] | [ ${colors.bold(`Type error`)} ] ${colors.red(`${Exception.mysqlErrorCon} : ${colors.bold(`ER_ACCESS_DENIED_ERROR`)}`)} - [ ${colors.bold(`Error message`)} ] ${colors.red(`${Exception.accessDeniedToDBCon(user!, password!)}`)} - [ ${colors.red(`${colors.bold(`HttpCode`)}`)} ] ${HttpStatusCodesConstant.UNAUTHORIZED} `)
             break;
         case "ER_BAD_DB_ERROR":
-            LoggerComponent.logErrorMessage(err.message, Exception.mysqlErrorCon);
-            throw new ExceptionHandlerError(
-                `${colors.bgRed(`${colors.white(err.message +`\n${Exception.unknownDB(database!)}`)}`)}`,
-                `${colors.red(Exception.mysqlErrorCon)}`,
-                HttpStatusCodesConstant.NOT_FOUND,
-                true
-            );
+            LoggerComponent.logErrorMessage(Exception.unknownDB(database!), Exception.mysqlErrorCon);
+            logErrorName();
+            console.log(`[ ${colors.red(`${colors.bold('DataBase connection')}`)} ] | ${dateTime} | [ ${colors.red(`${colors.bold(`error`)}`)} ] | [ ${colors.bold(`Type error`)} ] ${colors.red(`${Exception.mysqlErrorCon} : ${colors.bold(`ER_BAD_DB_ERROR`)}`)} - [ ${colors.bold(`Error message`)} ] ${colors.red(`${Exception.unknownDB(database!)}`)} - [ ${colors.red(`${colors.bold(`HttpCode`)}`)} ] ${HttpStatusCodesConstant.NOT_FOUND} `);
+            break;
         default:
-            LoggerComponent.logErrorMessage(err.message, Exception.mySQLError)
-            throw new ExceptionHandlerError(
-                `${colors.bgRed(`${colors.white(err.message)}`)}`,
-                `${colors.red("MysqlError")}`,
-                HttpStatusCodesConstant.GONE,
-                true
-            );
+            LoggerComponent.logErrorMessage(err.message, Exception.mySQLError);
+            console.log(`[ ${colors.red(`${colors.bold('DataBase connection')}`)} ] | ${dateTime} | [ ${colors.red(`${colors.bold(`error`)}`)} ] | [ ${colors.bold(`Type error`)} ] ${colors.red(`${Exception.mysqlErrorCon} : ${colors.bold(`MysqlError`)}`)} - [ ${colors.bold(`Error message`)} ] ${colors.red(`${err.message}`)} - [ ${colors.red(`${colors.bold(`HttpCode`)}`)} ] ${HttpStatusCodesConstant.GONE} `);
     }
+}
+
+function logErrorName() {
+    console.log(chalk.bgRed.white(''.padEnd(18, ' ')));
+    let msg: string = colors.bold(' Errors occurring ');
+    console.log(chalk.bgRed.white(msg.padEnd(17.6, ' ')));
+    console.log(chalk.bgRed.white(''.padEnd(18.5, ' ')));
 }
