6adbd9499c8cd84da676c3e3d5f1da8c6260e2d3
guyzoum77 <guyzoum7700@mail.com>
2024-08-12 17:54:44 +0000

    adding server app entry runing app as a function and call it into import index.ts

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ import {getAccessEnv} from "./domain/env/access.env";
 import {dateTimeFormattedUtils} from "./core/utils/dateTimeFormatted.utils";
 import {database} from "opticore-database";
 import {serverParams} from "./core/utils/serverParams.utils";
-import {setupServerConfig} from "./core/config/setupServer.config";
+import {setupServerConfig as server} from "./core/config/setupServer.config";
 
 
 export type { PoolClient, PoolConfig, CustomTypesConfig, ConnectionConfig };
@@ -71,7 +71,7 @@ export {
     mySQL,
     database,
     serverParams,
-    setupServerConfig,
+    server,
     Client,
     createLogger,
     format,
