dc161ae47f36951737885d88ebcf1e95fe0aff27
guyzoum77 <guyzoum7700@mail.com>
2024-08-30 19:33:55 +0000

    fixing server side working

--- src/core/config/setupServer.config.ts
+++ src/core/config/setupServer.config.ts
@@ -6,10 +6,9 @@ import {dateTimeFormattedUtils} from "../utils/dateTimeFormatted.utils";
 
 export function setupServerConfig() {
     const app: express.Application = express();
-    const appModules: NodeJS.Module[] | undefined = require.main?.children;
     const webServer: ServerListenUtils = new ServerListenUtils();
 
     const server = webServer.onStartEvent(app, getAccessEnv.appHost, Number(getAccessEnv.appPort));
-    this.webServer.onListeningEvent(server, app, getAccessEnv.appHost, Number(this.port), dateTimeFormattedUtils);
-    this.webServer.onRequestEvent(server, app, getAccessEnv.appHost, Number(this.port), dateTimeFormattedUtils);
+    webServer.onListeningEvent(server, app, getAccessEnv.appHost, Number(this.port), dateTimeFormattedUtils);
+    webServer.onRequestEvent(server, app, getAccessEnv.appHost, Number(this.port), dateTimeFormattedUtils);
 }
