d8a048508cfe10556b862afee32dbc2c4981e4b5
guyzoum77 <guyzoum7700@mail.com>
2024-08-30 19:34:23 +0000

    fixing server side working

--- src/core/config/setupServer.config.ts
+++ src/core/config/setupServer.config.ts
@@ -9,6 +9,6 @@ export function setupServerConfig() {
     const webServer: ServerListenUtils = new ServerListenUtils();
 
     const server = webServer.onStartEvent(app, getAccessEnv.appHost, Number(getAccessEnv.appPort));
-    webServer.onListeningEvent(server, app, getAccessEnv.appHost, Number(this.port), dateTimeFormattedUtils);
-    webServer.onRequestEvent(server, app, getAccessEnv.appHost, Number(this.port), dateTimeFormattedUtils);
+    webServer.onListeningEvent(server, app, getAccessEnv.appHost, Number(getAccessEnv.appPort), dateTimeFormattedUtils);
+    webServer.onRequestEvent(server, app, getAccessEnv.appHost, Number(getAccessEnv.appPort), dateTimeFormattedUtils);
 }
