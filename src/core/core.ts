099e46310980334f1a0e185eba48de5111d2dc10
guyzoum77 <guyzoum7700@mail.com>
2024-08-30 23:44:39 +0000

    fixing bug from loadKernel

--- src/core/core.ts
+++ src/core/core.ts
@@ -7,10 +7,11 @@ import {modulesLoadedUtils as loadedModules} from "./utils/modulesLoaded.utils";
 export async function loadKernel(kernel: any[]) {
     try {
         const routesApp: (Awaited<any>)[] = await Promise.all(kernel.map((loader: any) => loader())); //@ts-ignore
-        const routers = routesApp[0].registerRoutes;
-        routers().map((router: any) => { return router; }); //@ts-ignore
+
         const serverSide = routesApp[1].app;
         serverSide(); //@ts-ignore
+        const routers = routesApp[0].registerRoutes;
+        routers().map((router: any) => { return router; }); //@ts-ignore
         const db = routesApp[2].dbConnection;
         db();
 
