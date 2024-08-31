754e7bbdaee3c91cbd6a6d7489be13a453a893c9
guyzoum77 <guyzoum7700@mail.com>
2024-08-31 08:36:20 +0000

    fix error occurring for 'app.router' from uncaughtException and uncaughtExceptionMonitor process event

--- src/core/utils/serverListen.Utils.ts
+++ src/core/utils/serverListen.Utils.ts
@@ -366,7 +366,7 @@ export class ServerListenUtils {
             );
         });
         process.on(event.uncaughtException, (error: any) => {
-            if (error.name === '\'app.router\' is deprecated!\nPlease see the 3.x to 4.x migration guide for details on how to update your app.') {
+            if (error.message === '\'app.router\' is deprecated!\nPlease see the 3.x to 4.x migration guide for details on how to update your app.') {
                 console.log("");
             } else {
                 LogMessageUtils.error(
@@ -381,7 +381,7 @@ export class ServerListenUtils {
             }
         });
         process.on(event.uncaughtExceptionMonitor, (error: any) => {
-            if (error.name === '\'app.router\' is deprecated!\nPlease see the 3.x to 4.x migration guide for details on how to update your app.') {
+            if (error.message === '\'app.router\' is deprecated!\nPlease see the 3.x to 4.x migration guide for details on how to update your app.') {
                 console.log("");
             } else {
                 LogMessageUtils.error(
