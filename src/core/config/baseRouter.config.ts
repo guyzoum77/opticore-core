0b2f3510d775d0132e09be86c3cfa98ab311c3f0
guyzoum77 <guyzoum7700@mail.com>
2024-12-04 20:36:29 +0000

    ty to fixing undefined route

--- src/core/config/baseRouter.config.ts
+++ src/core/config/baseRouter.config.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 
-export class BaseRouter<T, U> {
+export class BaseRouterConfig<T, U> {
     public router: Router;
     public controller: T;
     public middleware: U;
