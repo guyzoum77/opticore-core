c54d49e3268f49b7a56195017ddc0aab19e67926
guyzoum77 <guyzoum7700@mail.com>
2024-12-05 23:05:11 +0000

    ty to fixing undefined route, and i setting up generic router class for register and feature module and finally i add express.Application and constructor

--- src/core/router/registerCoreRoute.router.ts
+++ src/core/router/registerCoreRoute.router.ts
@@ -1,6 +1,12 @@
 import express from "express";
 
 export class RegisterCoreRouteRouter {
+    private router: express.Application = express();
+
+    constructor(app: express.Application) {
+        this.router = app;
+    }
+
     routers (routes: express.Router[]): express.Router[] {
         return routes;
     }
