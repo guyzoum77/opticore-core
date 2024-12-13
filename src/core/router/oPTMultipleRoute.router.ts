4a939c35322dcdb282c038812a2b18d4aa3dec85
guyzoum77 <guyzoum7700@mail.com>
2024-12-13 23:28:11 +0000

    trying a new way (code) into oPTMultipleRouter class

--- src/core/router/oPTMultipleRoute.router.ts
+++ src/core/router/oPTMultipleRoute.router.ts
@@ -1,26 +1,49 @@
-import {BaseRouterConfig} from "@/core/config/baseRouter.config";
-import {TRouteConfigType} from "@/core/types/routeConfig.type";
-import {IRouterConfig} from "@/core/interfaces/routerConfig.interface";
+import { Router } from "express";
+import { BaseRouterConfig } from "@/core/config/baseRouter.config";
+import { TRouteConfigType } from "@/core/types/routeConfig.type";
+import { IRouteDefinition } from "@/core/interfaces/routeDefinition.interface";
+import {LogMessageUtils} from "@/core/utils/logMessage.utils";
+import { HttpStatusCodesConstant as status } from "@/domain/constants/httpStatusCodes.constant";
+import process from "node:process";
 
 export class oPTMultipleRouter<TController, TAuthenticator> extends BaseRouterConfig<TController, TAuthenticator> {
     private routeConfigs: TRouteConfigType[];
+    private readonly controller: TController;
+    private readonly basePath: string;
 
-    constructor(controller: new (...args: any[]) => TController,
-                authenticator: { new(...args: any[]): TAuthenticator } | null,
+    constructor(basePath: string = "/",
+                controller: new (...args: any[]) => TController,
+                authenticator: { new (...args: any[]): TAuthenticator } | null,
                 routeConfigs: TRouteConfigType[]) {
         super(controller, authenticator);
+        this.basePath     = basePath;
         this.routeConfigs = routeConfigs;
+        this.controller   = new controller();
     }
 
-    routes() {
+    routes(): IRouteDefinition[] {
+        const router = Router();
+
         this.routeConfigs.forEach((route: TRouteConfigType): void => {
             const { path, method, handler, middlewares } = route;
-            
-            middlewares
-                ? this.router[method](path, middlewares, handler)
-                : this.router[method](path, handler);
+
+            if (typeof this.controller[handler as keyof TController] !== "function") {
+                LogMessageUtils.error(
+                    "Handler",
+                    "Handler not found",
+                    process.cwd() + "/src/core/router/oPTMultipleRoute.router.ts",
+                    `Handler ${String(handler)} does not exist on the controller.`,
+                    status.NOT_FOUND
+                );
+                throw "";
+            }
+
+            const routeHandler = (this.controller[handler as keyof TController] as any).bind(this.controller);
+            middlewares && middlewares.length > 0
+                ? router[method](path, ...middlewares, routeHandler)
+                : router[method](path, routeHandler);
         });
 
-        return this.router;
+        return [{ path: this.basePath, handler: router }];
     }
 }
--- src/core/utils/logMessage.utils.ts
+++ src/core/utils/logMessage.utils.ts
@@ -6,15 +6,15 @@ export class LogMessageUtils {
         console.log(`${colors.green(`✔`)} ${colors.bgGreen(` ${colors.bold(`${colors.white(`${title}`)}`)} `)}  ${dateTimeFormattedUtils} | ${colors.bgGreen(`${colors.white(` Success `)}`)} [ ${action} ] ${contentAction} - [ Status ] ${colors.bgGreen(`${colors.white(` 200 `)}`)}`);
     }
     static warning(title: string, action: string, contentAction: string): void {
-        console.log(`${colors.yellow(`⚠️`)} ${colors.bgYellow(` ${colors.bold(`${colors.white(`${title}`)}`)} `)}  ${dateTimeFormattedUtils} | ${colors.bgYellow(`${colors.white(` Warning `)}`)} ${contentAction}`);
+        console.warn(`${colors.yellow(`⚠️`)} ${colors.bgYellow(` ${colors.bold(`${colors.white(`${title}`)}`)} `)}  ${dateTimeFormattedUtils} | ${colors.bgYellow(`${colors.white(` Warning `)}`)} ${contentAction}`);
     }
     static info(title: string, action: string, contentAction: string): void {
-        console.log(`${colors.cyan(`ⓘ`)} ${colors.bgCyan(` ${colors.bold(`${colors.white(`${title}`)}`)} `)}  ${dateTimeFormattedUtils} | ${colors.bgCyan(`${colors.white(` Info `)}`)} ${contentAction}`);
+        console.info(`${colors.cyan(`ⓘ`)} ${colors.bgCyan(` ${colors.bold(`${colors.white(`${title}`)}`)} `)}  ${dateTimeFormattedUtils} | ${colors.bgCyan(`${colors.white(` Info `)}`)} ${contentAction}`);
     }
     static error(title: string, errorType: any, stackTrace: any, messageContent: any, httpCodeValue: number): void {
-        console.log(`${colors.red(`✘`)} ${colors.bgRed(` ${colors.bold(`${colors.white(`${title}`)}`)} `)} | ${dateTimeFormattedUtils} | [ ${colors.red(`${colors.bold(` ${errorType} `)}`)} ] | [ ${colors.bold(`stack trace`)} ] ${colors.red(`${stackTrace}`)} - ${colors.red(`${messageContent}`)} - [ ${colors.red(`${colors.bold(` HttpCode `)}`)} ] ${colors.red(`${colors.bold(` ${httpCodeValue} `)}`)} `);
+        console.error(`${colors.red(`✘`)} ${colors.bgRed(` ${colors.bold(`${colors.white(`${title}`)}`)} `)} | ${dateTimeFormattedUtils} | [ ${colors.red(`${colors.bold(` ${errorType} `)}`)} ] | [ ${colors.bold(`stack trace`)} ] ${colors.red(`${stackTrace}`)} - ${colors.red(`${messageContent}`)} - [ ${colors.red(`${colors.bold(` HttpCode `)}`)} ] ${colors.red(`${colors.bold(` ${httpCodeValue} `)}`)} `);
     }
     static requestError(title: string, errorName: string, errorMessage: string, errorCode: number): void {
-        console.log(`[ ${colors.red(`${title}`)} ] ${dateTimeFormattedUtils} | ${colors.bgRed(`${colors.white(` ERROR `)}`)} ${colors.red(`[ ${errorName} ]`)} ${colors.red(`${errorMessage}`)} - [ Status ] ${colors.bgRed(`${colors.white(` ${errorCode} `)}`)}`);
+        console.error(`[ ${colors.red(`${title}`)} ] ${dateTimeFormattedUtils} | ${colors.bgRed(`${colors.white(` ERROR `)}`)} ${colors.red(`[ ${errorName} ]`)} ${colors.red(`${errorMessage}`)} - [ Status ] ${colors.bgRed(`${colors.white(` ${errorCode} `)}`)}`);
     }
 }
