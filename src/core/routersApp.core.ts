e02f623dbea558c08f2d966b06ea8af9005598e6
guyzoum77 <guyzoum7700@mail.com>
2025-01-14 13:41:34 +0000

    trying to fix an error coming from optMultipleRouter at authMiddleware function into getRoute method

--- src/core/routersApp.core.ts
+++ src/core/routersApp.core.ts
@@ -1,26 +1,7 @@
 import express from "express";
-import {PassportStrategy} from "@/core/middleware/strategies/passport.strategy";
-import {JwtPayload} from "jsonwebtoken";
 
 export class RoutersAppCore {
     static route(items: { path: string; handler: express.Router }[]) {
         return items;
-    }
-}
-
-export class ggr implements PassportStrategy {
-    private readonly pubKey: string;
-
-    jwtValidate(payload: JwtPayload, done: any): void {
-    }
-
-    async localValidate(): Promise<void> {
-        return Promise.resolve(undefined);
-    }
-
-    get useJWT(): void {
-    }
-
-    get useLocal(): void {
     }
 }
